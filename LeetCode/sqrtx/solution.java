0b424645d34d8ef406b4ce1138f895a7793be32a
Shibiraj R <124015659+shibi22@users.noreply.github.com>
2026-09-06 16:09:56 +0530

    Add solution: Sqrt(x)

---
+++ LeetCode/sqrtx/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if (x==0) return 0;
+       int  left = 1 , right = x , ans = 1;
+
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+
+                if(mid > x/mid){
+                    right = mid;
+                }
+
+
+                else{
+                    ans = mid;
+                    left = mid +1;
+                }
+        }
+
+        return ans;
+    }
+}
