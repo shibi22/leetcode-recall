2f6640bff84441ddfc05a1fce917b4ca248e8e77
Shibiraj R <124015659+shibi22@users.noreply.github.com>
2026-09-06 15:12:48 +0530

    Add solution: Find Peak Element

---
+++ LeetCode/find-peak-element/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+      int peak = 0;
+
+      for(int i = 1; i < nums.length; i++){
+        
+        if(nums[i] > nums[peak]){
+            peak = i;
+        }
+
+      } 
+
+      return peak;
+
+    }
+}
