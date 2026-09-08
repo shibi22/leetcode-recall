dc1196410bda0d19ff8d82448ed6126a08ccecf9
Shibiraj R <124015659+shibi22@users.noreply.github.com>
2026-09-08 23:29:32 +0530

    Add solution: Daily Temperatures

---
+++ LeetCode/daily-temperatures/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for(int i  = temperatures.length-1; i >=0 ; i--){
+            
+            while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]){
+                stack.pop();
+            }
+            
+            
+            if(!stack.isEmpty()){
+                result[i] =  stack.peek() - i;
+            }
+            
+            stack.push(i);
+        }
+        
+        
+        return result;
+    }
+}
