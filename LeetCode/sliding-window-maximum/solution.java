08b537fbef168a1def178e4d1a2e519b78d614cd
Shibiraj R <124015659+shibi22@users.noreply.github.com>
2026-09-10 22:45:27 +0530

    Add solution: Sliding Window Maximum

--- LeetCode/sliding-window-maximum/solution.java
+++ LeetCode/sliding-window-maximum/solution.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-
-        Deque<Integer> dq = new LinkedList<>();
         int n = nums.length;
-        int result[] = new int[n -k +1];
-        int ri = 0;
+        int[] result = new int[n - k + 1];
+        int resultIndex = 0;
+        Deque<Integer> deque = new LinkedList<>();
 
+        for(int i = 0; i < nums.length; i++){
 
-            for(int i =0; i<n; i++){
+        while(!deque.isEmpty() && deque.peekFirst() <=  i-k ){
+                deque.pollFirst();
+        }
+
+        while(!deque.isEmpty() && nums[deque.peekLast()] <= nums[i] ){
+                deque.pollLast();
+        }
             
-            while (!dq.isEmpty() && dq.peek() <i -k +1){
-                dq.pollFirst();
-            }
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
-                dq.pollLast();
-         }
-            dq.offerLast(i);
+            deque.offerLast(i);
 
-
-            if(i >= k -1 ){
-                result[ri++]= nums[dq.peekFirst()];
+            if(i >= k -1){
+                result[resultIndex++] = nums[deque.peekFirst()];
             }
-    
-            }
+        }
 
-            return result;        
+        return result;
     }
-
 }
