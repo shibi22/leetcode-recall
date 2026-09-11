97837fdca990dfc7a4daf3d2fea6f0ca0e098b2b
Shibiraj R <124015659+shibi22@users.noreply.github.com>
2026-09-11 20:13:38 +0530

    Add solution: Trapping Rain Water

---
+++ LeetCode/trapping-rain-water/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int left = 0 , right = n-1, maxLeft = 0 , maxRight = 0 , result = 0 ;
+
+
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] > maxLeft){
+                    maxLeft = height[left];
+                }
+
+                else {
+                    result += maxLeft - height[left];
+                }
+
+                left++;
+            }
+            else {
+                 if(height[right] > maxRight){
+                    maxRight = height[right];
+                }
+
+                else {
+                    result += maxRight - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
